0ea0c7f9f7ffd75b4eb0fc86ef88d7e9a22926e5
Paul Gross <pgross@maryville.edu>
2017-08-09 23:12:16 -0500

    Init

--- Emoji Responder/ViewController.swift
+++ Emoji Responder/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateResponse()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var emojiResponseLabel: UILabel!
+    
+    @IBOutlet weak var happySwitch: UISwitch!
+    @IBOutlet weak var tiredSwitch: UISwitch!
+    @IBOutlet weak var hungrySwitch: UISwitch!
+    
+    @IBAction func onFeelingChange(_ sender: UISwitch) {
+        updateResponse()
+    }
+    
+    func updateResponse() {
+        var emojiResponse:String
+        
+        if hungrySwitch.isOn {
+            emojiResponse = "🍕"
+        } else if !happySwitch.isOn && tiredSwitch.isOn {
+            emojiResponse = "☕️ or 🛌"
+        } else if happySwitch.isOn {
+            emojiResponse = "😁"
+        } else {
+            emojiResponse = "✌️"
+        }
+        
+        emojiResponseLabel.text = emojiResponse
+    }
 
 }
 
